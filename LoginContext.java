6119fd32361e13ea87aa47ad0de3d7d56111b690
Srinivas Saripalli <saripalli.srinivas@gmail.com>
2020-05-02 02:10:26 -0700

    Login Context File

---
+++ LoginContext.java
@@ -0,0 +1,24 @@
+/** @since 1.5 */
+public LoginContext (final String name, final Subject subject,
+                     final CallbackHandler cbHandler,
+                     Configuration config)
+  throws LoginException
+{
+  this.name = name;
+  this.subject = subject;
+  this.cbHandler = cbHandler;
+  if (config == null)
+    config = Configuration.getConfig();
+  AppConfigurationEntry[] entries = config.getAppConfigurationEntry (name);
+  if (entries == null)
+    entries = config.getAppConfigurationEntry (OTHER);
+  if (entries == null)
+    throw new LoginException ("no configured modules for application "
+                              + name);
+  this.entries = entries;
+  modules = new LoginModule[entries.length];
+  sharedState = new HashMap();
+  for (int i = 0; i < entries.length; i++)
+    modules[i] = lookupModule (entries[i], subject, sharedState);
+}
+ 

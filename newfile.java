c1b3478f26840e8f8f967a7edc65f91ef4a4366f
Srinivas Saripalli <saripalli.srinivas@gmail.com>
2020-05-02 22:14:01 -0700

    Neww file in new branch

---
+++ newfile.java
@@ -0,0 +1,6 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!-NeW FILE - JUNK FILE");
+	System.out.println("Hellow Srinivas");
+    }
+}
